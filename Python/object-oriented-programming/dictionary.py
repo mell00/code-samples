d77af40b18dfde2dafdfd06248cb8797016bf35b
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-02-10 09:42:09 -0800

    Addition of Python language dictionary code file

---
+++ Python/object-oriented-programming/dictionary.py
@@ -0,0 +1,21 @@
+#PYTHON DICTIONARIES#
+
+#Create a dictionary:
+
+dictionary_name = {
+
+key_1:value_1
+key_2:value_2
+key_3:value_3
+key_4:value_4
+...
+}
+
+#Return entire content of a dictionary:
+dict(dictionary_name)
+##Returns contents of dictionary_name##
+
+#Return specified key value within a dictionary:
+dictionary_name[key_name]
+#OR
+dictionary_name[index_number]
