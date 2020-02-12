c280b7b9807aea03423fb84677f7fcc459e377a2
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-02-12 09:46:43 -0800

    Added introductory data type content to JavaScript language oop file

---
+++ JavaScript/object-oriented-programming/oop.js
@@ -0,0 +1,34 @@
+//OBJECT-ORIENTED JAVASCRIPT PROGRAMMING//
+
+//Define and assign value to a variable in one of three ways:
+var variable_name1 = "variable_value1";
+const variable_name2 = "variable_value2";
+//NOTE: the value of a variable set with const cannot be altered
+let variable_name3 = "variable_value3";
+
+//Update value of a variable:
+variable_name1 = "variable_value4";
+
+//Define and assign value(s) to an array:
+let array_name1 = ["array_value1","array_value2","array_value3"];
+let array_name2 = [1, 2, 3];
+
+//Access value within an array:
+array_name1[0]
+//"array_value1"
+
+//Define and assign key:value pairs to an object:
+const object_name1 = {key_1:"value_1", key_2:[1,2], key_3:"value_3"};
+
+//Call the property of an object:
+object_name1.key_1
+//"value_1"
+
+//Access value of a (collection containing) property called on an object:
+object_name1.key_2[0]
+//1
+
+//Define a function:
+function function_1 (param1,param2) {
+
+}
