b270cd103cef38757fa4cf4743ed8dc834bec756
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-03-01 00:25:15 -0800

    Addition of C folder and C variables and expressions file

---
+++ C/variables-and-expressions.c
@@ -0,0 +1,13 @@
+//C VARIABLES AND EXPRESSIONS//
+
+#include <stdio.h>
+
+function_name()
+{
+  //insert code here
+}
+
+variable_1 = 3;
+variable_2 = "a string";
+variable_3 = 3.1;
+variable_4 = '\n\n\"\?'
