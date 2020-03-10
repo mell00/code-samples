8106b8bc528873a8b22a801cf9dd7683157d9e2e
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-03-10 15:58:31 -0700

    Additions to ajax file (w/o descriptions)

---
+++ JavaScript/world-wide-web/ajax.js
@@ -0,0 +1,10 @@
+//AJAX BASICS//
+
+var xhttp = newXMLHttpRequest();
+
+xhttp.onreadystatechange = function () {
+  if(xhttp.readyState === 4) {executed_code_in_response_to_finished_request}};
+
+xhttp.open(method,url,async,user,psw);
+
+JSON.parse('string')
