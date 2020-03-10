de92dd058a53148df10ab4e3ed4aa5ea21646717
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-03-09 23:15:27 -0700

    Initialization of & additions to statistical analysis file

---
+++ MATLAB/statistical-analysis.m
@@ -0,0 +1,68 @@
+&&STATISTICAL ANALYSIS WITH MATLAB%%
+
+A = [1 2 3 4 5 6]
+
+cumprod Cumulative product
+cumsum Cumulative sum
+cumtrapz Cumulative trapezoidal numerical integration
+factor Prime factors
+inpolygon Detect points inside a polygonal region
+max(A)
+%returns 6
+
+mean(A)
+%returns 3.5
+
+median(A)
+%returns 3.5
+
+min(A)
+%returns 1
+
+perms All possible permutations
+polyarea Area of polygon
+primes(A)
+%returns 1 2 3 5
+
+prod(A)
+%returns 720
+
+rectint Rectangle intersection Area
+sort Sort elements in ascending order
+sortrows Sort rows in ascending order
+std Standard deviation
+sum(A)
+%returns 21
+trapz Trapezoidal numerical integration
+var Variance
+corrcoef Correlation coefficients
+cov Covariance matrix
+conv Convolution and polynomial multiplication
+conv2 Two-dimensional convolution
+deconv Deconvolution and polynomial division
+filter Filter data with an infinite impulse response (IIR) or finite impulse response (FIR) filter
+filter2 Two-dimensional digital filtering
+convhull Convex hull
+convhulln Multidimensional convex hull
+delaunay Delaunay triangulation
+delaunay3 Three-dimensionalDelaunay tessellation
+delaunayn Multidimensional Delaunay tessellation
+dsearch Search for nearest point
+dsearchn Multidimensional closest point search
+griddata Data gridding
+griddata3 Data gridding and hypersurface fitting for three-dimensional data
+griddatan Data gridding and hypersurface fitting (dimension >= 2)
+interp1 One-dimensional data interpolation (table lookup)
+interp2 Two-dimensional data interpolation (table lookup)
+interp3 Three-dimensional data interpolation (table lookup)
+interpft One-dimensional interpolation using the FFT method
+interpn Multidimensional data interpolation (table lookup)
+meshgrid Generate X and Y matrices for three-dimensional plots
+ndgrid Generate arrays for multidimensional functions and interpolation
+pchip Piecewise Cubic Hermite Interpolating Polynomial (PCHIP)
+ppval Piecewise polynomial evaluation
+spline Cubic spline data interpolation
+tsearch Search for enclosing Delaunay triangle
+tsearchn Multidimensional closest simplex search
+voronoi Voronoi diagram
+voronoin Multidimensional Voronoi diagrams
