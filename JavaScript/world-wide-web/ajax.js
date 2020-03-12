aaa791dd9e4fe16e16d9e802bb5609bd4f19e2e2
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-03-12 14:59:11 -0700

    Addition of AJAX basics sample code

--- JavaScript/world-wide-web/ajax.js
+++ JavaScript/world-wide-web/ajax.js
@@ -1,1 +1,87 @@
 //AJAX BASICS//
+
+xhttp.open("GET", "demo_get.asp", true);
+xhttp.send();
+
+xhttp.open("GET", "demo_get.asp?t=" + Math.random(), true);
+xhttp.send();
+
+xhttp.open("GET", "demo_get2.asp?fname=Henry&lname=Ford", true);
+xhttp.send();
+
+xhttp.open("POST", "demo_post.asp", true);
+xhttp.send();
+
+xhttp.open("POST", "ajax_test.asp", true);
+xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhttp.send("fname=Henry&lname=Ford");
+
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("demo").innerHTML = this.responseText;
+  }
+};
+xhttp.open("GET", "ajax_info.txt", true);
+xhttp.send();
+
+
+
+
+
+
+loadDoc("url-1", myFunction1);
+
+loadDoc("url-2", myFunction2);
+
+function loadDoc(url, cFunction) {
+  var xhttp;
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      cFunction(this);
+    }
+  };
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
+
+function myFunction1(xhttp) {
+  // action goes here
+}
+function myFunction2(xhttp) {
+  // action goes here
+}
+
+
+document.getElementById("demo").innerHTML = xhttp.responseText;
+
+
+xmlDoc = xhttp.responseXML;
+txt = "";
+x = xmlDoc.getElementsByTagName("ARTIST");
+for (i = 0; i < x.length; i++) {
+  txt += x[i].childNodes[0].nodeValue + "<br>";
+}
+document.getElementById("demo").innerHTML = txt;
+xhttp.open("GET", "cd_catalog.xml", true);
+xhttp.send();
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("demo").innerHTML =
+    this.getAllResponseHeaders();
+  }
+};
+
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("demo").innerHTML =
+    this.getResponseHeader("Last-Modified");
+  }
+};
+xhttp.open("GET", "ajax_info.txt", true);
+xhttp.send();
