f9de2e7c96363ebdcba847de6aac219a68c9e64f
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-03-12 01:39:35 -0700

    Split AJAX code samples from AJAX tutorial code file

---
+++ JavaScript/world-wide-web/ajax-applications.js
@@ -0,0 +1,12 @@
+//AJAX APPLICATION CODE SAMPLES//
+
+
+
+var xhttp = newXMLHttpRequest();
+
+xhttp.onreadystatechange = function () {
+  if(xhttp.readyState === 4) {executed_code_in_response_to_finished_request}};
+
+xhttp.open(method,url,async,user,psw);
+
+JSON.parse('string')
