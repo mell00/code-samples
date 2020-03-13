b361001050d703ca55c1e9456b03f3cba8981e13
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-03-13 16:07:23 -0700

    Edit description syntax

--- MATLAB/elem-math-functions.m
+++ MATLAB/elem-math-functions.m
@@ -1,41 +1,120 @@
-//MATLAB ELEMENTARY MATH FUNCTIONS//
+%MATLAB ELEMENTARY MATH FUNCTIONS%
 
-cos(x)
+%Arithmetic functions:%
+abs(x) %absolute value of x%
 
-sin(x)
+sign(x) %signum of x%
 
-tan(x)
+max(x) %maximum value of x%
 
-acos(x)
+min(x) %minimum value of x%
 
-asin(x)
+ceil(x) %x rounded towards +∞%
 
-atan(x)
+floor(x) %x rounded towards −∞%
 
-exp(x)
+round(x) %x rounded to nearest integer%
 
-sqrt(x)
+rem(x) %remainder of x after division%
 
-log(x)
+mod(x) %remainder of x after division (modulo operation)%
 
-log10(x)
+conj(x) %complex conjugate of x%
 
-abs(x)
 
-sign(x)
+%Exponential/Logarithmic functions:%
+exp(x) %exponential of x%
 
-max(x)
+sqrt(x) %square root of x%
 
-min(x)
+log(x) %natural logarithm of x%
 
-ceil(x)
+log10(x) %common (base-10) logarithm of x%
 
-floor(x)
 
-round(x)
+%Trigonometric functions:%
+cos(x) %cosine of x in radians%
 
-rem(x)
+cosd(x) %cosine of x in degrees%
 
-angle(x)
+sin(x) %sine of x in radians%
 
-conj(x)
+sind(x) %sine of x in degrees%
+
+tan(x) %tangent of x in radians%
+
+tand(x) %tangent of x in degrees%
+
+acos(x) %arc cosine of x in radians%
+
+acosd(x) %arc cosine of x in degrees%
+
+asin(x) %arc sine of x in radians%
+
+asind(x) %arc sine of x in degrees%
+
+atan(x) %arc tangent of x in radians%
+
+atand(x) %arc tangent of x in degrees%
+
+csc(x) %cosecant of x in radians%
+
+cscd(x) %cosecant of x in degrees%
+
+acsc(x) %arc cosecant of x in radians%
+
+acscd(x) %arc cosecant of x in degrees%
+
+sec(x) %secant of x in radians%
+
+secd(x) %secant of x in degrees%
+
+asec(x) %arc secant of x in radians%
+
+asecd(x) %arc secant of x in degrees%
+
+cot(x) %cotangent of x in radians%
+
+cotd(x) %cotangent of x in degrees%
+
+acot(x) %arc cotangent of x in radians%
+
+acotd(x) %arc cotangent of x in degrees%
+
+hypot(x,y) &square root of sum of squares of x and y; hypotenuse of right triangle lengths x and y%
+
+angle(x) %phase angle of complex number x%
+
+deg2rad(x) %conversion of angle value x from degrees to radians%
+
+rad2deg(x) %conversion of angle value x from radians to degrees%
+
+%Hyperbolic Trigonometric functions (in radians):%
+sinh(x) %hyperbolic sine of x%
+
+asinh(x) %inverse hyperbolic sine of x%
+
+cosh(x) %hyperbolic cosine of x%
+
+acosh(x) %inverse hyperbolic cosine of x%
+
+tanh(x) %hyperbolic tangent of x%
+
+atanh(x) %inverse hyperbolic tangent of x%
+
+csch(x) %hyperbolic cosecant of x%
+
+acsch(x) %inverse hyperbolic cosecant of x%
+
+sech(x) %hyperbolic secant of x%
+
+asech(x) %inverse hyperbolic secant of x%
+
+coth(x) %hyperbolic cotangent of x%
+
+acoth(x) %inverse hyperbolic cotangent of x%
+
+%Discrete functions:%
+factor(x) %prime factor(s) of x%
+
+factorial(x) %factorial of x%
