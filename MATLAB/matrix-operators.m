d1761d511d9150b27dca002d1ae70e72028c3d13
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-03-13 21:28:02 -0700

    Addition of basic matrix operations, properties, and structural code

---
+++ MATLAB/matrix-operators.m
@@ -0,0 +1,99 @@
+%MATRIX OPERATIONS IN MATLAB%
+
+transpose(A) %nonconjugate transpose of A%
+
+ctranspose(A) %complex conjugate transpose of A%
+
+mtimes(A,B) %matrix product of A and B%
+OR
+A*B
+
+mpower(A,B) %A to the power of B%
+OR
+A^B
+
+sqrtm(A) %principal square root of A%
+
+expm(A) %matrix exponential of A%
+
+logm(A) %principal matrix logarithm of A%
+
+funm(A,function,options,p1,p2,..) %user-defined function at square matrix argument A%
+
+kron(A,B) %Kronecker tensor product of A and B%
+
+cross(A,B,dim) %cross product of A and B along optional dimension dim%
+
+dot(A,B,dim) %dot product of A and B along optional dimension dim%
+
+norm()
+
+normest()
+
+vecnorm()
+
+cond()
+
+condest()
+
+rcond()
+
+condeig()
+
+det()
+
+null()
+
+orth()
+
+rank()
+
+rref()
+
+trace()
+
+subspace()
+
+size()
+
+zeros()
+
+ones()
+
+diag()
+
+horzcat()
+
+eye()
+
+inv()
+
+kron()
+
+vecnorm()
+
+pascal()
+
+magic()
+
+randi()
+
+bandwidth()
+
+tril()
+
+triu()
+
+isbanded()
+
+isdiag()
+
+ishermitian()
+
+issymmetric()
+
+istril()
+
+istriu()
+
+isempty()
