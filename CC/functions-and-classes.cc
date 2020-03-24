1c49f3b2f7029e876baab2df9e53878b3cf26e04
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-03-23 22:45:24 -0700

    Add function creation section; function return type section in progress

--- CC/functions-and-classes.cc
+++ CC/functions-and-classes.cc
@@ -1,1 +1,20 @@
-//C++
+//C++ FUNCTIONS AND CLASSES//
+
+#include <stdio.h>
+#include <filename.h> OR "filename.h"
+
+//Creating a function:
+return_type function_name(type p1, type p2, .. ,type pn){
+  //Code to be executed;
+}
+
+//Function return types:
+
+    //void
+    void function(){
+      //Code to be executed;
+      return //value;
+    }
+
+    //auto
+    
