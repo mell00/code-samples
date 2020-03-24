c8ec09fb07389ec2ed04da27bea6c0b13ccce310
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-03-23 22:05:08 -0700

    Add dataset layout reshaping tools

--- Python/data-science/pandas/data-manipulation.py
+++ Python/data-science/pandas/data-manipulation.py
@@ -10,4 +10,31 @@
     #Rename columns of a dataframe (df):
     df.rename(columns={'new_column_name':'current_column_name'})
 
+    df.sort_index()
+
+    df.reset_index()
+
+    df.drop(columns=['length','height'])
+
+    #Spread rows into columns:
+    df.pivot(columns='var',values='val')
+
+    #Append rows of dataframes:
+    pd.concat([df1,df2])
+
+    #Append columns of dataframes:
+    pd.concat([df1,df2],axis=1)
+
+    #Gather columns into rows:
+    pd.melt(df)
+
+#Select rows
     
+
+
+
+
+
+
+
+#Select columns
