79bdde0e84df446d42dfd70adbadb92bc1c0811e
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-03-24 17:16:50 -0700

    Add column creation, vector function, combo data set, and filtered joins

--- Python/data-science/pandas/data-manipulation.py
+++ Python/data-science/pandas/data-manipulation.py
@@ -19,14 +19,49 @@
     #Spread rows into columns:
     df.pivot(columns='var',values='val')
 
-    #Append rows of dataframes:
+    #Add rows of dataframes:
     pd.concat([df1,df2])
 
     #Append columns of dataframes:
     pd.concat([df1,df2],axis=1)
 
+    #Add a single column multiplying values of existing columns:
+    df['new_column']=df.existing_column1*df.existing_column2*df.existing_columnn
+
+    #Compute and append one or more new columns:
+    df.assign({'new_column':[column_val1,column_val2,..,column_valn]},index=['row_1','row_2',..,'row_n']) #dictionary of values of new_column, matched with index values (rows)
+    df.assign(new_column=df['existing_column'] **kwargs) #value of 'existing_column's evaluated on df
+    OR
+    df.assign(new_column=lambda x: x['existing_column'] **kwargs) #value of 'existing_column's evaluated on df by substitution of variable x as df
+        #NOTE: callable may be re-used for multiple columns
+
+    #Column vector functions (operate on either one selected column or all columns):
+        #Find element-wise maximum of selected column(s)
+        df.max(axis=1)
+        #Find element-wise minimum of selected column(s)
+        df.min(axis=1)
+        #Find absolute value of selected column(s)
+        df.abs()
+        #Trim values of selected column(s) at given input thresholds
+        df.clip(lower=low_val,higher=high_val)
+
     #Gather columns into rows:
     pd.melt(df)
+
+    #Combine data sets:
+    pd.merge(df1,df2,how='left',on='column_n')
+
+    pd.merge(df1,df2,how='right',on='column_n')
+
+    pd.merge(df1,df2,how='inner',on='column_n')
+
+    pd.merge(df1,df2,how='outer',on='column_n')
+
+    #Filter joins:
+        #Filter rows of df1 that have a match in df2
+        df1[df1.column_n.isin(df2.column_n)]
+        #Filter rows of df1 that do not have a match in df2
+        df1[~df1.column_n.isin(df2.column_n)]
 
 #Select rows
     #Select rows by (slice) position:
@@ -80,3 +115,37 @@
         '^word' #matches strings beginning with the word 'word'
         'x[1-5]' #matches strings beginning with x and ending with 1,2,3,4,5
         '(^!string)' #matches strings except the string 'string'
+
+#Group data
+    #Return a GroupBy object, grouped by values in column ('col'):
+    df.groupby(by='col')
+
+    #Return a GroupBy object, grouped by values in index level ('ind'):
+    df.groupby(by='ind')
+
+    #Return value of group size:
+    df.size()
+
+    #Return aggregated group by function:
+    df.agg(function)
+
+    #Return group with values shifted forward by 1:
+    df.shift(1)
+
+    #Return group with values shifted behind by 1:
+    df.shift(-1)
+
+    #Return ranked group by 'method' and either rescaled or not rescaled:
+    df.rank(method='method',pct=boolean)
+
+    #Return cumulative sum of group values:
+    df.cumsum()
+
+    #Return cumulative maximum value of group:
+    df.cummax()
+
+    #Return cumulative minimum value of group:
+    df.cummin()
+
+    #Return cumulative product of group values:
+    df.comprod()
