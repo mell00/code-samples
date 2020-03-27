a83abe38818a848b5d1114692553e53a6efbda3a
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-03-27 01:40:36 -0700

    Convert to Shell file; add virtualenv console commands

---
+++ Python/console/virtualenv.sh
@@ -0,0 +1,25 @@
+#PYTHON virtualenv#
+
+$pip3 install virtualenv
+
+$pip3 list
+
+$mkdir directory_for_virtualenvs
+
+$cd directory_for_virtualenvs
+
+$ls directory_for_virtualenvs
+
+$virtualenv project_virtenv_name
+
+$source project_virtenv_name/bin/activate
+
+$which python
+
+$deactivate
+
+$rm -rf project_virtenv_name
+
+$virtualenv -p /usr/bin/python_version project_virtenv_name
+
+$python --version
