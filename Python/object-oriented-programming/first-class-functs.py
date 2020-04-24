f5565ff479f660f9bbb07e4f492c19749ec0dfbe
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-04-23 23:28:37 -0700

    Add function to custom map()

--- Python/object-oriented-programming/first-class-functs.py
+++ Python/object-oriented-programming/first-class-functs.py
@@ -20,3 +20,30 @@ print(variable)
 ##function function at 0x101278950##
 print(variable(4))
 ##16##
+
+#Create built-in map() function:
+
+
+#Create custom map() function (custom_map), using the function created above:
+def custom_map(function,params):
+    result = []
+    for item in params:
+        result.append(function(item))
+    return result
+
+var = custom_map(function,[1,2,3,4,5])
+print(var)
+##[1,4,9,16,25]##
+
+#Pass in an alternate function into the custom map() function (custom_map):
+def function_2(param):
+    return param * param * param
+def custom_map(function,params):
+    result = []
+    for item in params:
+        result.append(function(item))
+    return result
+
+var_2 = custom_map(function_2,[1,2,3,4,5])
+print(var_2)
+##[1,8,27,64,125]##
