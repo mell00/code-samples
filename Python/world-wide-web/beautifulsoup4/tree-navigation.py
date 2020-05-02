552b713863cf82059f70b71dd66a2ba38995f9cf
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-05-02 14:43:15 -0700

    Add outline of tree nav code

--- Python/world-wide-web/beautifulsoup4/tree-navigation.py
+++ Python/world-wide-web/beautifulsoup4/tree-navigation.py
@@ -28,3 +28,37 @@ soup.find_all('tag_name')
     #ex:
     soup.find_all('a')
     ##<a href=https://www.google.com/>Google</a><a href=https:https://github.com/>GitHub</a>##
+
+#Navigation by contents() (list of all children of said tag name):
+
+
+
+#Navigation by children() (iterable sequence of all children of said tag name):
+
+#Navigation by descendants() ():
+
+#Navigation by string() ():
+
+#Navigation by strings() ():
+
+#Navigation by stripped_strings() ():
+
+#Navigation by parent() ():
+
+#Navigation by parents() ():
+
+#Navigation by next_sibling() ():
+
+#Navigation by next_siblings() ():
+
+#Navigation by previous_sibling() ():
+
+#Navigation by previous_siblings() ():
+
+#Navigation by next_element() ():
+
+#Navigation by next_elements() ():
+
+#Navigation by previous_element() ():
+
+#Navigation by previous_elements() ():
