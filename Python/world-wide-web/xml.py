7dfa88f65baff8b6b98b2df604d748fc521eb76e
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-05-12 00:02:05 -0700

    Add remaining node package code

--- Python/world-wide-web/xml.py
+++ Python/world-wide-web/xml.py
@@ -31,3 +31,17 @@ node.nodeValue
 node.hasAttributes()
 
 node.hasChildNodes()
+
+node.isSameNode(other)
+
+node.appendChild(new_child)
+
+node.insertBefore(new_child,reference_child)
+
+node.removeChild(old_child)
+
+node.replaceChild(new_child,old_child)
+
+node.normalize()
+
+node.cloneNode(deep)
