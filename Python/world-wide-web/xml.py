6fd7a74bb3a86c611c8b5859069846adb79df436
Madison Ell <54490369+mell00@users.noreply.github.com>
2020-05-11 23:58:25 -0700

    Add part of node package code

---
+++ Python/world-wide-web/xml.py
@@ -0,0 +1,33 @@
+##PYTHON XML PROCESSING SUPPORT##
+
+#node package
+
+node.nodeType
+
+node.parentNode
+
+node.attributes
+
+node.previousSibling
+
+node.nextSibling
+
+node.childNodes
+
+node.firstChild
+
+node.lastChild
+
+node.localName
+
+node.prefix
+
+node.namespaceURI
+
+node.nodeName
+
+node.nodeValue
+
+node.hasAttributes()
+
+node.hasChildNodes()
